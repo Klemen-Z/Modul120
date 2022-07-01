84357621cfca750942f8e70c74b8eba6c31a8404
Klemen-Z <klemenzmahar@gmail.com>
2022-07-01 13:31:49 +0200

    some smol fix

--- Pages/login.php
+++ Pages/login.php
@@ -19,7 +19,7 @@
     <body>
         <div class="m-auto mt-[5%] p-2 min-h-fit min-w-fit rounded-lg w-[30rem] h-[80%] bg-slate-800">
             <div class="text-gray-300 min-h-fit min-w-fit w-full h-full">
-                <div class="flex flex-cols-1 flex-rows-4 flex-col h-full items-center gap-y-12">
+                <form class="flex flex-cols-1 flex-rows-4 flex-col h-full items-center gap-y-12">
                     <svg class="w-24 h-24 ml-auto mr-auto stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
@@ -32,10 +32,10 @@
                         <input id="userP" type="Password" placeholder="Password" class="w-full rounded bg-slate-800 pl-1 text-gray-300 border-solid border-gray-400" />
                     </div>
                     <div class="grow shrink bottom-2 pt-[70%]">
-                        <a class="cursor-pointer rounded-md p-1 my w-24 h-12 text-lg font-semibold ml-auto mr-auto text-center bg-slate-500 text-gray-300 inset-x-0 bottom-0" href="login.php?log=true">Log in</a>
+                        <Button type="submit" class="cursor-pointer rounded-md p-1 my w-24 h-12 text-lg font-semibold ml-auto mr-auto text-center bg-slate-500 text-gray-300 inset-x-0 bottom-0">Log in</Button>
                         <a class="cursor-pointer rounded-md p-1 my w-24 h-12 text-lg font-semibold ml-auto mr-auto text-center bg-slate-500 text-gray-300 inset-x-0 bottom-0" href="register.php">Register</a>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </body>
